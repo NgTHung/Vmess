c153c8586b13316c55e4b2730f12dacf3da7f5e0
NgTHung <tuanhungag1@gmail.com>
2022-09-17 21:50:42 +0700

    presistence state

--- frontend/src/components/loginForm.svelte
+++ frontend/src/components/loginForm.svelte
@@ -1,20 +1,44 @@
 <script>
+  import { onMount } from "svelte";
+  import {tryLogin, isLoggedIn, tryLogout} from "../store/user"
+  let cred = {username:"",password:""}
+  const login = async (e) =>{
+    await tryLogin(cred)
+    if($isLoggedIn == "true"){
+      location.replace("/")
+    }
+    else{
+      console.log("ERROR!")
+    }
+  }
+  onMount(()=>{
+    if($isLoggedIn == "true"){
+      location.replace('/')
+    }
+  })
+  const logout = async ()=>{
+    await tryLogout()
+  }
 </script>
-<div>
-	<span>
-	  <p class="title">Đăng nhập tài khoảng</p>
-	  <!-- <div class="big"> -->
-	  <form>
-		  <input type="text" placeholder="Tên tài khoảng..." />
-		  <input type="password" placeholder="Mật khẩu..." />
-		  <div class="center">
-			  <input type="submit" value="Đăng nhập" class="submit" />
-		  </div>
-	  </form>
-	</span>
-</div>
-<!-- </div> -->
 
+<div>
+  {$isLoggedIn}
+  <button on:click={logout}>Logout</button>
+  <article>Vmess</article>
+  <span>
+    <p class="title">Đăng nhập tài khoảng</p>
+    <!-- <div class="big"> -->
+    <form on:submit|preventDefault={login}>
+      <input type="text" placeholder="Tên tài khoảng..." bind:value={cred.username} />
+      <input type="password" placeholder="Mật khẩu..." bind:value={cred.password} />
+      <div class="center">
+        <input type="submit" value="Đăng nhập" class="submit" />
+      </div>
+    </form>
+  </span>
+</div>
+
+<!-- </div> -->
 <style>
   form {
     width: auto;
@@ -29,9 +53,9 @@
     flex-shrink: 0; */
     /* margin: 40px; */
   }
-  .center{
-	display: flex;
-	justify-content: center;
+  .center {
+    display: flex;
+    justify-content: center;
   }
   input[type="submit"] {
     margin: 1px;
@@ -45,9 +69,9 @@
   }
   span {
     position: absolute;
-	bottom: 0px;
-	width: auto;
-	display: inline-block;
+    bottom: 0px;
+    width: auto;
+    display: inline-block;
   }
   input[type="text"],
   input[type="password"] {
@@ -67,9 +91,9 @@
     color: black;
   }
 
-  div{
-	display: flex;
-	justify-content: center;
+  div {
+    display: flex;
+    justify-content: center;
   }
   .title {
     width: auto;
@@ -79,7 +103,7 @@
     font-weight: bold;
     text-decoration: underline;
     font-size: large;
-	display: flex;
-	justify-content: center;
+    display: flex;
+    justify-content: center;
   }
 </style>
--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -1,30 +1,28 @@
-import { atom, action, task } from "nanostores";
+import { task } from "nanostores";
 import * as axios from "axios"
-export const isLoggedIn = atom(false);
-export const mutateIsLoggedIn = action(
-    isLoggedIn,
-    "mutateIsLoggedIn",
-    (store, payload) => {
-        store.set(payload);
-        return store.get();
-    }
-);
+import {persistentAtom} from "@nanostores/persistent"
+
+
+export const isLoggedIn = persistentAtom<string>("sadas","false");
 
 export const tryLogin = async (objCredential: any) => {
     await task(async () => {
         const res = await axios.default.post("http://localhost:3000/auth/login", { username: objCredential.username, password: objCredential.password }, { withCredentials: true })
         if (res.data === 'Done') {
-            mutateIsLoggedIn(true)
+            isLoggedIn.set("true")
+        }
+        else{
+            console.log(res)
         }
     });
 };
 
 export const tryLogout = async () => {
     await task(async () => {
-        if (!isLoggedIn.get()) return;
+        // if (!isLoggedIn.get()) return;
         const res = await axios.default.get("http://localhost:3000/auth/logout", { withCredentials: true })
         if (res.data === 'Done') {
-            mutateIsLoggedIn(false)
+            isLoggedIn.set("false")
         }
     })
 }
